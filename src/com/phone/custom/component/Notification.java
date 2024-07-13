60fe4d8fa1451206e17b9ccfdc276bf55d82e725
hatundev <hatun35.dev@gmail.com>
2024-07-13 20:04:32 +0700

    create notification panel

---
+++ src/com/phone/custom/component/Notification.java
@@ -0,0 +1,291 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
+ */
+package com.phone.custom.component;
+
+import com.phone.custom.shadow.ShadowRenderer;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+import javax.swing.JDialog;
+import org.jdesktop.animation.timing.Animator;
+import org.jdesktop.animation.timing.TimingTarget;
+import org.jdesktop.animation.timing.TimingTargetAdapter;
+
+/**
+ *
+ * @author hatun
+ */
+public class Notification extends javax.swing.JComponent {
+
+    private JDialog dialog;
+    private Animator animator;
+    private final Frame fram;
+    private boolean showing;
+    private Thread thread;
+    private int animate = 10;
+    private BufferedImage imageShadow;
+    private int shadowSize = 6;
+    private Type type;
+    private Location location;
+
+    /**
+     * Creates new form Notification
+     */
+    public Notification(Frame fram, Type type, Location location) {
+        this.fram = fram;
+        this.type = type;
+        this.location = location;
+        initComponents();
+        init();
+        initAnimator();
+    }
+
+    private void init() {
+        setBackground(Color.WHITE);
+        dialog = new JDialog(fram);
+        dialog.setUndecorated(true);
+        dialog.setFocusableWindowState(false);
+        dialog.setBackground(new Color(0, 0, 0, 0));
+        dialog.add(this);
+        dialog.setSize(getPreferredSize());
+        if (type == Type.SUCCESS) {
+            lbIcon.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/phone/resources/icons/success.png")));
+            lbMessage.setText("Thành công!");
+        } else {
+            lbIcon.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/phone/resources/icons/erros.png")));
+            lbMessage.setText("Thất bại!");
+        }
+    }
+
+    private void initAnimator() {
+        TimingTarget target = new TimingTargetAdapter() {
+            private int x;
+            private int top;
+            private boolean top_to_bot;
+
+            @Override
+            public void timingEvent(float fraction) {
+                if (showing) {
+                    float alpha = 1f - fraction;
+                    int y = (int) ((1f - fraction) * animate);
+                    if (top_to_bot) {
+                        dialog.setLocation(x, top + y);
+                    } else {
+                        dialog.setLocation(x, top - y);
+                    }
+                    dialog.setOpacity(alpha);
+                } else {
+                    float alpha = fraction;
+                    int y = (int) (fraction * animate);
+                    if (top_to_bot) {
+                        dialog.setLocation(x, top + y);
+                    } else {
+                        dialog.setLocation(x, top - y);
+                    }
+                    dialog.setOpacity(alpha);
+                }
+            }
+
+            @Override
+            public void begin() {
+                if (!showing) {
+                    dialog.setOpacity(0f);
+                    int margin = 10;
+                    int y = 0;
+                    if (location == Location.TOP_CENTER) {
+                        x = fram.getX() + ((fram.getWidth() - dialog.getWidth()) / 2);
+                        y = fram.getY();
+                        top_to_bot = true;
+                    } else if (location == Location.TOP_RIGHT) {
+                        x = fram.getX() + fram.getWidth() - dialog.getWidth() - margin;
+                        y = fram.getY() + 5 * margin;
+                        top_to_bot = true;
+                    } else if (location == Location.TOP_LEFT) {
+                        x = fram.getX() + margin;
+                        y = fram.getY();
+                        top_to_bot = true;
+                    } else if (location == Location.BOTTOM_CENTER) {
+                        x = fram.getX() + ((fram.getWidth() - dialog.getWidth()) / 2);
+                        y = fram.getY() + fram.getHeight() - dialog.getHeight();
+                        top_to_bot = false;
+                    } else if (location == Location.BOTTOM_RIGHT) {
+                        x = fram.getX() + fram.getWidth() - dialog.getWidth() - margin;
+                        y = fram.getY() + fram.getHeight() - dialog.getHeight();
+                        top_to_bot = false;
+                    } else if (location == Location.BOTTOM_LEFT) {
+                        x = fram.getX() + margin;
+                        y = fram.getY() + fram.getHeight() - dialog.getHeight();
+                        top_to_bot = false;
+                    } else {
+                        x = fram.getX() + ((fram.getWidth() - dialog.getWidth()) / 2);
+                        y = fram.getY() + ((fram.getHeight() - dialog.getHeight()) / 2);
+                        top_to_bot = true;
+                    }
+                    top = y;
+                    dialog.setLocation(x, y);
+                    dialog.setVisible(true);
+                }
+            }
+
+            @Override
+            public void end() {
+                showing = !showing;
+                if (showing) {
+                    thread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            sleep();
+                            closeNotification();
+                        }
+                    });
+                    thread.start();
+                } else {
+                    dialog.dispose();
+                }
+            }
+        };
+        animator = new Animator(500, target);
+        animator.setResolution(5);
+    }
+
+    public void showNotification() {
+        animator.start();
+    }
+
+    private void closeNotification() {
+        if (thread != null && thread.isAlive()) {
+            thread.interrupt();
+        }
+        if (animator.isRunning()) {
+            if (!showing) {
+                animator.stop();
+                showing = true;
+                animator.start();
+            }
+        } else {
+            showing = true;
+            animator.start();
+        }
+    }
+
+    private void sleep() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+        }
+    }
+
+    @Override
+    public void paint(Graphics grphcs) {
+        Graphics2D g2 = (Graphics2D) grphcs.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(getBackground());
+        g2.drawImage(imageShadow, 0, 0, null);
+        int x = shadowSize;
+        int y = shadowSize;
+        int width = getWidth() - shadowSize * 2;
+        int height = getHeight() - shadowSize * 2;
+        g2.fillRect(x, y, width, height);
+//        if (type == Type.SUCCESS) {
+//            g2.setColor(new Color(18, 163, 24));
+//        } else if (type == Type.INFO) {
+//            g2.setColor(new Color(28, 139, 206));
+//        } else {
+//            g2.setColor(new Color(241, 196, 15));
+//        }
+        g2.fillRect(6, 5, 5, getHeight() - shadowSize * 2 + 1);
+        g2.dispose();
+        super.paint(grphcs);
+    }
+
+    @Override
+    public void setBounds(int i, int i1, int i2, int i3) {
+        super.setBounds(i, i1, i2, i3);
+        createImageShadow();
+    }
+
+    private void createImageShadow() {
+        imageShadow = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = imageShadow.createGraphics();
+        g2.drawImage(createShadow(), 0, 0, null);
+        g2.dispose();
+    }
+
+    private BufferedImage createShadow() {
+        BufferedImage img = new BufferedImage(getWidth() - shadowSize * 2, getHeight() - shadowSize * 2, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = img.createGraphics();
+        g2.fillRect(0, 0, img.getWidth(), img.getHeight());
+        g2.dispose();
+        return new ShadowRenderer(shadowSize, 0.3f, new Color(100, 100, 100)).createShadow(img);
+    }
+
+    /**
+     * This method is called from within the constructor to initialize the form.
+     * WARNING: Do NOT modify this code. The content of this method is always
+     * regenerated by the Form Editor.
+     */
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        lbIcon = new javax.swing.JLabel();
+        cmdClose = new javax.swing.JButton();
+        lbMessage = new javax.swing.JLabel();
+
+        lbIcon.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lbIcon.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/phone/resources/icons/success.png"))); // NOI18N
+
+        cmdClose.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/phone/resources/icons/close.png"))); // NOI18N
+        cmdClose.setBorder(null);
+        cmdClose.setContentAreaFilled(false);
+        cmdClose.setFocusable(false);
+
+        lbMessage.setFont(new java.awt.Font("Liberation Sans", 1, 14)); // NOI18N
+        lbMessage.setText("Thành công");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(lbIcon)
+                .addGap(20, 20, 20)
+                .addComponent(lbMessage, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(20, 20, 20)
+                .addComponent(cmdClose)
+                .addGap(20, 20, 20))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(lbIcon, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(cmdClose, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(20, 20, 20))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(28, 28, 28)
+                .addComponent(lbMessage)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+    }// </editor-fold>//GEN-END:initComponents
+    public static enum Type {
+        SUCCESS, WARNING
+    }
+
+    public static enum Location {
+        TOP_CENTER, TOP_RIGHT, TOP_LEFT, BOTTOM_CENTER, BOTTOM_RIGHT, BOTTOM_LEFT, CENTER
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton cmdClose;
+    private javax.swing.JLabel lbIcon;
+    private javax.swing.JLabel lbMessage;
+    // End of variables declaration//GEN-END:variables
+}
