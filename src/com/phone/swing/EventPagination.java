0196728fb5b2a0ac347385005e30141798bfbc41
hatundev <hatun35.dev@gmail.com>
2024-07-15 16:49:01 +0700

    upload swing

---
+++ src/com/phone/swing/EventPagination.java
@@ -0,0 +1,5 @@
+package com.phone.swing;
+
+public interface EventPagination {
+     public void pageChanged(int page);
+}
---
+++ src/com/phone/swing/PaginationItemRenderStyle1.java
@@ -0,0 +1,22 @@
+package com.phone.swing;
+
+import javax.swing.JButton;
+
+public class PaginationItemRenderStyle1 extends DefaultPaginationItemRender {
+     @Override
+    public JButton createButton(Object value, boolean isPrevious, boolean isNext, boolean enable) {
+        JButton button = super.createButton(value, isPrevious, isNext, enable);
+        button.setUI(new ButtonUI());
+        return button;
+    }
+
+    @Override
+    public Object createPreviousIcon() {
+        return "Previous";
+    }
+
+    @Override
+    public Object createNextIcon() {
+        return "Next";
+    }
+}
